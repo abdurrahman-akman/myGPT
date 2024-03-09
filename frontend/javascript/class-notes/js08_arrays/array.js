b79aedabcc21c65310a23b3c7172819d58661361
AbdurRahman <a.akman4444@gmail.com>
2024-03-09 17:19:06 +0300

    referans-adress

--- frontend/javascript/class-notes/js08_arrays/array.js
+++ frontend/javascript/class-notes/js08_arrays/array.js
@@ -27,10 +27,10 @@
 // console.log(araymidir instanceof Array) //false
 
 //! str immutable oldugu için değiştirlemez
-let str = 'yellow-blue'
-console.log(str[0])
-str[0] = 'b'
-console.log(str)
+// let str = 'yellow-blue'
+// console.log(str[0])
+// str[0] = 'b'
+// console.log(str)
 
 //!arraylaer ise mutable oldugu için değiştirilebilir.
 // const colors = ['red', 'purple', 'black', 'yellow-lacivert']
@@ -45,3 +45,28 @@ console.log(str)
 // colors[100] = 4
 // console.log(colors)
 
+// let a = 4
+// let b = a
+// console.log(a)
+// console.log(b)
+// a=6
+// console.log(b)
+
+
+// let arr1 = [1, 2, 'three', false]
+// const arr2 = arr1
+// console.log(arr1)
+// console.log(arr2)
+// arr1[0] = 0
+// console.log(arr1)
+// console.log(arr2)
+
+// let c = ['2']
+// console.log(Boolean(c))
+// let d = ['2']
+// console.log(c == d)
+
+// arr1 = ['four']
+// console.log(arr1)
+// arr2 = ['four']
+// console.log(arr2)
